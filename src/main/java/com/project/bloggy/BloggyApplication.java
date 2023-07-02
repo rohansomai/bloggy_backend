5b10a7be6a74d78a4ef90f4db330b77c7fe5af38
Rohan Somai <rohansomai6@gmail.com>
2023-07-02 23:51:18 +0530

    added swagger api docs

--- src/main/java/com/project/bloggy/BloggyApplication.java
+++ src/main/java/com/project/bloggy/BloggyApplication.java
@@ -5,17 +5,28 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableSwagger2
 public class BloggyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BloggyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BloggyApplication.class, args);
+    }
 
-	@Bean
-	public ModelMapper modelMapper(){
-		return new ModelMapper();
-	}
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    @Bean
+    public Docket bloggyApi() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("com.project.bloggy")).build();
+    }
 }
